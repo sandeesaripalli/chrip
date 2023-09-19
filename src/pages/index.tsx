9d13e95939081d11dbccd9e57ed5e4fbe7d10da9
sandeepsaripalli <hawks900@gmail.com>
2023-09-19 11:46:55 -0400

    added posts router and deleted some dummy code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,10 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 export default function Home() {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  // const hello = api.example.hello.useQuery({ text: "from tRPC" });
   const user = useUser()
+  const {data} = api.posts.getAll.useQuery()
+  console.log(data)
   return (
     <>
       <Head>
@@ -23,9 +25,9 @@ export default function Home() {
           {!user.isSignedIn && <SignInButton />}
           {!!user.isSignedIn && <SignOutButton />}
           </div>
-          <p className="text-2xl text-white">
-            {hello.data ? hello.data.greeting : "Loading tRPC query..."}
-          </p>
+          <div>
+            {data?.map((post) => (<div key={post.id}>{post.content}</div>))}
+          </div>
         </div>
       </main>
     </>
